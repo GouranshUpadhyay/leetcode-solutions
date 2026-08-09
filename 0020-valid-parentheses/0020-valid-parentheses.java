33d551c3b400fd4fdf418f390b22d80487763c6c
GouranshUpadhyay <gouranshupadhyay457@gmail.com>
2026-08-09 23:09:52 +0530

    Time: 3 ms (86%), Space: 43.1 MB (73.61%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+
+            // Closing bracket
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+
+        // If nothing is left, everything matched
+        return stack.isEmpty();
+    }
+}
