fcb53d02715f75807b5fe4829b11033f85c50a75
GouranshUpadhyay <gouranshupadhyay457@gmail.com>
2026-09-02 13:56:41 +0530

    Time: 3 ms (86.19%), Space: 43.5 MB (15.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,41 +1,18 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-
-            // Opening brackets
-            if (ch == '(' || ch == '[' || ch == '{') {
+        Deque<Character> stack=new ArrayDeque<>();
+        for(char ch:s.toCharArray()){
+            if( ch=='(' || ch=='{' || ch=='['){
                 stack.push(ch);
             }
-
-            // Closing bracket
-            else {
-
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                char top = stack.pop();
-
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
+            else{
+                if(stack.isEmpty()) return false;
+                char top=stack.pop();
+                if(ch==')' && top!='(') return false;
+                if(ch=='}' && top!='{') return false;
+                if(ch==']' && top!='[') return false;
             }
         }
-
-        // If nothing is left, everything matched
         return stack.isEmpty();
     }
 }
